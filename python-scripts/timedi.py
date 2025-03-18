35cb71069f34913a384087b9edcaec3a3edcd852
Jordan Totten <39886184+tottenjordan@users.noreply.github.com>
2025-03-18 17:55:41 +0800

    Update timedi.py

--- python-scripts/timedi.py
+++ python-scripts/timedi.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Oct 31 19:12:21 2018
-
 @author: jtotten
 """
 
@@ -11,12 +9,12 @@ def main():
 	running = True
 
 	while running:
-
-		print('Enter a speed in m/s or as a fraction of the speed of light.')
+        print('Enter a speed in m/s or as a fraction of the speed of light.')
 		print('m/s in the format: x m/s, fraction in the format 0.x')
 
 		speed = input()
-		# regexes to check what format the input is in
+
+        # check input format
 		fraction = r'0\.[0-9]+'
 		raw_speed = r'[0-9]+[ ]*m/s'
 		is_fraction = re.search(fraction, speed)
@@ -24,16 +22,17 @@ def main():
 
 		# if the input is in a correct format
 		if is_fraction or is_raw_speed:
-
-			if is_raw_speed:
-				# remove m/s part of the string and any spaces
+            
+            if is_raw_speed:
+                # remove m/s part of the string and any spaces
 				speed = float(speed.split('m/s')[0].strip(' '))
 				input_format = 'r'
 			elif is_fraction:
 				# convert fraction as a string to float
 				speed = float(speed)
 				input_format = 'f'
-			# use calc_time_dilation to calculate the amount of time dilation
+			
+            # use calc_time_dilation to calculate the amount of time dilation
 			# between an obe
 			dilation = calc_time_dilation(speed, speed_format=input_format)
 			
@@ -43,7 +42,7 @@ def main():
 			again = input()
 			
 			if again.lower() == 'q':
-				running = False
+                running = False
 		else:
 			print('Invalid format')
 
@@ -61,12 +60,10 @@ def calc_time_dilation(speed, speed_format='r'):
 	else:
 		raise ValueError("Speed format must be a string r or f -- raw speed or fraction")
 	
-
 	ratio_to_c = (speed ** 2) / (c ** 2)
 	dilation = 1 / ((1 - ratio_to_c) ** 0.5)
 	return dilation
 
+
 if __name__ == '__main__':
 	main()
-    
-cal
